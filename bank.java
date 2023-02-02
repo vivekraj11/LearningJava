ff287f48756027c53ae341ae4b37dbbcb3235df1
vivekraj11 <123090360+vivekraj11@users.noreply.github.com>
2023-02-02 19:28:47 +0530

    Add files via upload

---
+++ bank.java
@@ -0,0 +1,17 @@
+package MTsir;
+
+public class bank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RBI c1=new SBI();
+		c1.accHolderName();
+		c1.money();
+		RBI c2=new ICICI();
+		c2.accHolderName();
+		c2.money();
+		
+
+	}
+
+}
